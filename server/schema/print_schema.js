5cfcce1b2e478e852d817db2212f9f071e3e8d15
Raffi Hovagimian <rhovagimian@fb.com>
2020-07-19 16:15:39 -0400

    Update db

--- server/schema/print_schema.js
+++
@@ -1,8 +0,0 @@
-//@ts-check
-const { printSchema } = require("graphql");
-const fs = require("fs");
-const schema = require("./schema");
-const fileData = printSchema(schema);
-fs.writeFile("./server/schema/schema.graphql", fileData, (error) =>
-  console.error(error)
-);
